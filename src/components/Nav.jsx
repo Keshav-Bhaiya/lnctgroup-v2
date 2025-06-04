1330e20c34df43663c071b4e63bdc93d61fd24cf
My <keshavb266@gmail.com>
2025-06-04 19:50:23 +0530

    New commit

--- src/components/Nav.jsx
+++ src/components/Nav.jsx
@@ -31,7 +31,7 @@ const Header = () => {
   ];
 
   return (
-    <header className="bg-white shadow-sm sticky top-0 z-50">
+    <header className="bg-white shadow-sm fixed  left-0 w-full z-[100]">
       <div className="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8">
         <div className="flex justify-between items-center h-16">
           {/* Logo */}
